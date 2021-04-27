fa34edfbaa07d9e2779ffe113bec8ae7a01a8a72
kotomi <kotomi.okawa@gmail.com>
2021-04-27 15:06:12 +0900

    カレンダーやり直し　最初と最後の空欄の数を取得した

--- resources/js/components/action/Action.vue
+++ resources/js/components/action/Action.vue
@@ -4,16 +4,15 @@
 
         <!-- ページャー -->
         <div class="pager">
-            <router-link v-if="month == 1" class="pager_prev" :to="`/action/${Number(year)-1}/12`">＜</router-link>
-            <router-link v-if="month != 1" class="pager_prev" :to="`/action/${year}/${Number(month)-1}`">＜</router-link>
+            <router-link v-if="month == 1"  :to="`/action/${Number(year)-1}/12`">＜</router-link>
+            <router-link v-if="month != 1"  :to="`/action/${year}/${Number(month)-1}`">＜</router-link>
             <h3 class="cmn_pageTitle">{{ year }}年 {{ month }}月</h3>
-            <router-link v-if="month == 12" class="pager_next" :to="`/action/${Number(year)+1}/1`">＞</router-link>
-            <router-link v-if="month != 12" class="pager_next" :to="`/action/${year}/${Number(month)+1}`">＞</router-link>
+            <router-link v-if="month == 12"  :to="`/action/${Number(year)+1}/1`">＞</router-link>
+            <router-link v-if="month != 12"  :to="`/action/${year}/${Number(month)+1}`">＞</router-link>
         </div>
 
         <!-- カレンダー作成 -->
-        <form v-on:submit.prevent="submit">
-
+        <div class="calendar">
             <!-- 曜日表示 -->
             <ul class="indent">
                 <li class="indent_item">月</li>
@@ -26,22 +25,23 @@
             </ul>
 
             <!-- 日付表示 -->
-            <ul class="content">
-                <li v-for="(week, index) in calendars" :key="index" >
-                    <ul>
-                        <li v-for="(day, index) in week" :key="index">
-                            {{ day.date }}
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <!-- <ul class="content">
+                <li class="content_item blank"></li>
 
-        </form>
+                <li v-for="calendar in calendars" :key="calendar.date" class="content_item main">
+                    <span class="content_item_d"></span>
+                </li>
+
+                <li class="content_item blank"></li>
+            </ul> -->
+
+        </div>
         <!-- カレンダー↑↑↑↑ -->
 
         <!-- <pre>{{$data}}</pre> -->
     </div>
 </template>
+
 
 <script>
 import moment from "moment";
@@ -51,8 +51,10 @@ export default {
         return {
             year: 0,
             month: 0,
-
             actions: [],
+            // calendars: [],
+            first_cnt: 0,
+            last_cnt: 0,
         };
     },
     methods: {
@@ -62,52 +64,27 @@ export default {
         },
         actionRead() {
             axios.get("/api/action/read").then((res) => {
-                // console.log(res.data);
                 this.actions = res.data;
             });
         },
 
         // カレンダー作成
-        getStartDate() {
-            let date = moment().startOf("month");
-            const youbiNum = date.day();
-            return date.subtract(youbiNum - 1, "days");
+        // ①最初と最後の空欄の数を求める
+        getCalendar(){
+            this.first_cnt = moment().startOf('month').day() -1; 
+            this.last_cnt =(7-(moment().endOf('month').day()))%7; 
+            console.log(this.first_cnt);
+            console.log(this.last_cnt);
         },
-        getEndDate() {
-            let date = moment().endOf("month");
-            const youbiNum = date.day();
-            return date.add(7 - youbiNum, "days");
-        },
-        getCalendar() {
-            let startDate = this.getStartDate();
-            const endDate = this.getEndDate();
-            const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
-            // ↑週数を取得
 
-            let calendars = [];
-            for (let week = 0; week < weekNumber; week++) {
-                let weekRow = [];
-                for (let day = 0; day < 7; day++) {
-                    weekRow.push({
-                        date: startDate.get("date"),
-                    });
-                    startDate.add(1, "days");
-                }
-                calendars.push(weekRow);
-            }
-            return calendars;
-        },
     },
     mounted() {
         this.getDate();
         this.actionRead();
-        console.log(this.getCalendar());
+        this.getCalendar();
+
     },
-    computed: {
-        calendars() {
-            return this.getCalendar();
-        },
-    },
+
     watch: {
         $route: "getDate",
     },
@@ -159,26 +136,6 @@ form {
         }
     }
 }
-.content{
-    // max-width:900px;
-    border-top:1px solid grey;
-    li{
-        list-style: none;
-        // border-left:1px solid grey;
-        ul{
-            display: flex;
-            li{
-                flex:1;
-                min-height:125px;
-                border-left:1px solid grey;
-                border-bottom:1px solid grey;
-                padding: 5px 3px 3px 5px;
-                &:nth-child(7n) {
-                border-right: 1px solid grey;
-                }
-            }
-        }
-    }
-}
+
 
 </style>
